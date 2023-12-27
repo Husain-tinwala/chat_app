47172e57737eeceaf77c34cd99207019a5a45a5a
HussainTinwala <codezilla.hussain@gmail.com>
2023-12-27 15:59:01 +0530

    added gitignore

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class ChatModel(models.Model):
     message= models.TextField(null=True,blank=True)
     thread_name= models.CharField(null=True,blank=True,max_length=50)
     timestamp= models.DateTimeField(auto_now_add=True)
-
+     
     def __str__(self):
         return self.message
 
